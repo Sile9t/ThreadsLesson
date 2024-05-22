75fb642e244a1739277320a46c70818ed33863d6
Sile9t <eyesless1jack@gmail.com>
2024-05-22 18:33:44 +0300

    git init

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace ThreadsLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string site = "ya.ru";
+            //get available site addresses to ping them
+            var IPAddresses = Dns.GetHostAddresses(site, AddressFamily.InterNetwork);
+            foreach (var item in IPAddresses)
+                Console.WriteLine(item);
+            
+            var pings = new Dictionary<IPAddress, long>();
+            var threads = new List<Thread>();
+            //adding available addresses with reply time in dictionary
+            foreach (var address in IPAddresses)
+            {
+                var thread = new Thread(() =>
+                {
+                    var ping = new Ping();
+                    var reply = ping.Send(address);
+                    pings.Add(address, reply.RoundtripTime);
+                    Console.WriteLine($"{address} : {reply.RoundtripTime}");
+                });
+                threads.Add(thread);
+                thread.Start();
+            }
+            //pinging addresses
+            foreach (var thread in threads)
+                thread.Join();
+            var minPing = pings.Min(x => x.Value);
+            Console.WriteLine($"Min ping = {minPing}");
+        }
+    }
+}
